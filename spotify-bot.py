23607fbcca02990bab0fce3b28667c1a37aa50f9
Richelle Shim <richelleshim@gmail.com>
2024-12-01 19:21:54 -0800

    pls pls pls plsplsplsplsplsplsplspls 🎵 Currently listening to: BIRDS OF A FEATHER by Billie Eilish

--- spotify-bot.py
+++ spotify-bot.py
@@ -3,11 +3,10 @@ from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-
 # Load environment variables
 load_dotenv()
 
-# Spotify API credentials
+# Spotify API credentials from environment variables
 SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
 SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
 SPOTIPY_REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
@@ -17,12 +16,12 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     client_id=SPOTIPY_CLIENT_ID,
     client_secret=SPOTIPY_CLIENT_SECRET,
     redirect_uri=SPOTIPY_REDIRECT_URI,
-    scope="user-read-currently-playing"
+    scope="user-read-currently-playing user-read-playback-state"
 ))
 
 def get_current_song():
     current_track = sp.current_playback()
-    if current_track and current_track['is_playing']:
+    if current_track and current_track.get('is_playing'):
         song = current_track['item']['name']
         artist = ', '.join(artist['name'] for artist in current_track['item']['artists'])
         return f"{song} by {artist}"
